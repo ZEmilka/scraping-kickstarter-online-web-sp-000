e78ae8f438d1466d95ca6e9d53d1dc3dc7cbe7ec
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-14 00:31:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,6 @@
 # require libraries/modules here
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html') 
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 end
